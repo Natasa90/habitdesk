e8184de372cec8225a9a24f05f0ec03682d84952
Natasa90 <natasa.golubovic90@gmail.com>
2025-03-20 05:29:14 -0500

    upper case fixed

--- components/userProfileElements/index.tsx
+++ components/userProfileElements/index.tsx
@@ -1,6 +1,6 @@
 import { ToDoList } from "./ToDoList";
-import { UserProfileButtons } from "./UserProfileButtons";
-import { UserProfileTitle } from "./UserProfileTitle";
+import { UserProfileButtons } from "./userProfileButtons";
+import { UserProfileTitle } from "./userProfileTitle";
 import { UserWeeklyGoals } from "./WeeklyGoals/UserWeeklyGoals";
 
 export { ToDoList, UserProfileButtons, UserProfileTitle, UserWeeklyGoals };
