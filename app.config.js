6da0015ee48a629c31217362becc654b8d94bbd1
Natasa90 <natasa.golubovic90@gmail.com>
2025-03-26 12:10:10 -0500

    Added app.config.js for environment variables

---
+++ app.config.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  expo: {
+    name: "habitdesk",
+    slug: "habitdesk",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.habitdesk",
+      buildNumber: "1",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+        pushNotifications: true,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/slavoio-logo.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.anonymous.habitdesk",
+    },
+    scheme: "habitdesk",
+    platforms: ["ios", "android", "web"],
+    extra: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+      ascAppId: process.env.ASC_APP_ID,
+      easProjectId: process.env.EAS_PROJECT_ID,
+      ascKeyName: process.env.ASC_KEY_NAME,
+      ascKeyId: process.env.ASC_KEY_ID,
+      eas: {
+        projectId: process.env.EAS_PROJECT_ID,
+      },
+    },
+  },
+};
